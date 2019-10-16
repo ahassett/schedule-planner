0bf31f8b1daf1acc987efa6a70ea6d7f036b012e
Aiko Hassett <ahassett@middlebury.edu>
2019-10-16 17:56:36 -0400

    scrapped classes into array

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const fs = require('fs');
+const request = require('request');
+const cheerio = require('cheerio');
+
 const app = express();
 
 // setting port for running Express server
@@ -9,5 +13,32 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route -- to be able to fetch from client
 app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+
+    url = 'http://www.middlebury.edu/academics/catalog/depts'
+
+    // NOTE: structure of request call
+    // param 1 - url
+    // param 2 - callback function w error, repsose status, and html
+    request(url, function (error, response, html) {
+        if (!error){
+            const $ = cheerio.load(html);
+
+            // defining the variables we want to capture.
+            let department, classname, description, termsOffered, link;
+            let json = { department : "", classname: "", description: "", termsOffered: "", link: "" };
+            classLinks = []
+
+            // push every link found with <a> tag into array
+            $('.field-items a').each( (index, value) => {
+                let link = $(value).attr('href');
+                if(link.includes('/academics/') && !link.includes('catalog') && !link.includes('handbook')) {
+                    classLinks.push(link);
+                }
+            })
+
+
+                            console.log(classLinks)
+        }
+    })
+  res.send({ express: 'helo' });
 });
