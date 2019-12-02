2d05ba068539be952ca9418fc7cfc93ec446362b
Angel Ngulumbi <angulumbi@middlebury.edu>
2019-12-01 20:44:23 -0500

    color on schedule is consistent

--- client/src/components/ScheduleList.js
+++ client/src/components/ScheduleList.js
@@ -7,6 +7,38 @@ import star from './star.svg';
 import email_icon from './email_icon.svg';
 import delete_icon from './delete_icon.svg';
 
+function convertHrs(pasthrs, hrs, mins) {
+  //let temp_mins = mins
+  let first_shade, second_shade, new_hr = 0
+  first_shade = parseInt(mins)
+
+  second_shade = 60 - first_shade
+
+  first_shade = (first_shade / 60) * 100
+  first_shade = first_shade + '%'
+
+  if (second_shade >= hrs) {
+    second_shade = hrs
+  } else {
+    new_hr = hrs - second_shade
+    second_shade = 60
+  }
+
+  second_shade = (second_shade / 60) * 100
+  second_shade = second_shade + '%'
+
+  if (pasthrs === 0) {
+    return {first: first_shade, second: second_shade, third: new_hr}
+  }
+
+  if (pasthrs > 60) {
+    return {first: ((pasthrs / 60) * 100) + '%' , second: first_shade, third: pasthrs - 60}
+  }
+
+  return {first: ((pasthrs / 60) * 100) + '%' , second: first_shade, third: hrs - (60 - parseInt(mins))}
+
+}
+
 class ScheduleList extends Component {
     constructor(props) {
       super(props)
@@ -65,9 +97,7 @@ class ScheduleList extends Component {
       let header = Object.keys(this.state.itemArray[0])
       header.splice(0, 1);
       header.splice(0, 0, '');
-      header.splice(6, 1);
-      header.splice(6, 1);
-      console.log(header);
+      header.splice(6, 3);
       return header.map((key, index) => {
         return <th key={index}>{key}</th>
       })
@@ -75,8 +105,9 @@ class ScheduleList extends Component {
 
     // contents of the schedule-table
     renderTableData() {
-      let col_content, rand
-      let col_array = ['#ffc0cb', '#ffa500', '#00ffff', '#b300b3', '#228B22']
+      let col_content, pasthrs = {Mon: 0, Tues: 0, Wed: 0, Thurs: 0, Fri: 0}, hr_percent = {Mon: 0, Tues: 0, Wed: 0, Thurs: 0, Fri: 0}
+      let col_array = ['#ffc0cb', '#4287f5', '#a337e6', '#d82feb', '#ed5170', '#ffa500', '#00ffff', '#b300b3', '#228B22']
+      let past_classId = {Mon: 0, Tues: 0, Wed: 0, Thurs: 0, Fri: 0}
 
       if (this.state.col_contents) {
         col_content = "small_cols"
@@ -85,41 +116,160 @@ class ScheduleList extends Component {
       }
 
       return this.state.itemArray.map((itemArray, index) => {
-        const { time, Mon, Tues, Wed, Thurs, Fri, class_id } = itemArray // destructuring
+        const { time, Mon, Tues, Wed, Thurs, Fri, class_id, hrs, mins } = itemArray // destructuring
 
         let mon_color = '#ffffff', tues_color = '#ffffff', wed_color = '#ffffff', thurs_color = '#ffffff', fri_color = '#ffffff'
-
-        // use to fin color for each class
-        rand = Math.floor(Math.random() * 5)
+        let mon_color1 = '#ffffff', tues_color1 = '#ffffff', wed_color1 = '#ffffff', thurs_color1 = '#ffffff', fri_color1 = '#ffffff'
+        let mon_color2 = '#ffffff', tues_color2 = '#ffffff', wed_color2 = '#ffffff', thurs_color2 = '#ffffff', fri_color2 = '#ffffff'
 
         if (Mon !== '') {
-          mon_color = col_array[class_id]
+          if (pasthrs.Mon !== 0) {
+            mon_color1 = col_array[past_classId.Mon]
+            mon_color2 = col_array[class_id.Mon]
+          } else {
+            mon_color = col_array[class_id.Mon]
+          }
+
+          if (!hrs.Mon) {
+            hr_percent.Mon = convertHrs(pasthrs.Mon, 60, '00')
+            pasthrs.Mon = hr_percent.Mon.third
+          } else {
+            hr_percent.Mon = convertHrs(pasthrs.Mon, hrs.Mon, mins.Mon)
+            pasthrs.Mon = hr_percent.Mon.third
+          }
+
+          past_classId.Mon = class_id.Mon
+
+        } else {
+          if (pasthrs.Mon !== 0) {
+            mon_color1 = col_array[past_classId.Mon]
+          }
+
+          hr_percent.Mon = convertHrs(pasthrs.Mon, 60, '00')
+          pasthrs.Mon = hr_percent.Mon.third
         }
 
         if (Tues !== '') {
-          tues_color = col_array[class_id]
+          if (pasthrs.Tues !== 0) {
+            tues_color1 = col_array[past_classId.Tues]
+            tues_color2 = col_array[class_id.Tues]
+          } else {
+            tues_color = col_array[class_id.Tues]
+          }
+
+          if (!hrs.Tues) {
+            hr_percent.Tues = convertHrs(pasthrs.Tues, 60, '00')
+            pasthrs.Tues = hr_percent.Tues.third
+          } else {
+            hr_percent.Tues = convertHrs(pasthrs.Tues, hrs.Tues, mins.Tues)
+
+            pasthrs.Tues = hr_percent.Tues.third
+          }
+
+          past_classId.Tues = class_id.Tues
+
+        } else {
+          if (pasthrs.Tues !== 0) {
+            tues_color1 = col_array[past_classId.Tues]
+          }
+
+          hr_percent.Tues = convertHrs(pasthrs.Tues, 60, '00')
+          pasthrs.Tues = hr_percent.Tues.third
+
         }
 
         if (Wed !== '') {
-          wed_color = col_array[class_id]
+          if (pasthrs.Wed!== 0) {
+            wed_color1 = col_array[past_classId.Wed]
+            wed_color2 = col_array[class_id.Wed]
+          } else {
+            wed_color = col_array[class_id.Wed]
+          }
+
+          if (!hrs.Wed) {
+            hr_percent.Wed = convertHrs(pasthrs.Wed, 60, '00')
+            pasthrs.Wed = hr_percent.Wed.third
+          } else {
+            hr_percent.Wed = convertHrs(pasthrs.Wed, hrs.Wed, mins.Wed)
+            pasthrs.Wed = hr_percent.Wed.third
+          }
+
+          past_classId.Wed = class_id.Wed
+
+
+        } else {
+          if (pasthrs.Wed !== 0) {
+            wed_color1 = col_array[past_classId.Wed]
+          }
+
+          hr_percent.Wed = convertHrs(pasthrs.Wed, 60, '00')
+          pasthrs.Wed = hr_percent.Wed.third
         }
 
         if (Thurs !== '') {
-          thurs_color = col_array[class_id]
+          if (pasthrs.Thurs !== 0) {
+            thurs_color1 = col_array[past_classId.Thurs]
+            thurs_color2 = col_array[class_id.Thurs]
+          } else {
+            thurs_color = col_array[class_id.Thurs]
+          }
+
+          past_classId.Thurs = class_id.Thurs
+
+
+          if (!hrs.Thurs) {
+            hr_percent.Thurs = convertHrs(pasthrs.Thurs, 60, '00')
+            pasthrs.Thurs = hr_percent.Thurs.third
+          } else {
+            hr_percent.Thurs = convertHrs(pasthrs.Thurs, hrs.Thurs, mins.Thurs)
+            pasthrs.Thurs = hr_percent.Thurs.third
+          }
+
+        } else {
+          if (pasthrs.Thurs !== 0) {
+            thurs_color1 = col_array[past_classId.Thurs]
+          }
+
+          hr_percent.Thurs = convertHrs(pasthrs.Thurs, 60, '00')
+          pasthrs.Thurs = hr_percent.Thurs.third
+
         }
 
         if (Fri !== '') {
-          fri_color = col_array[class_id]
+          if (pasthrs.Fri !== 0) {
+            fri_color1 = col_array[past_classId.Fri]
+            fri_color2 = col_array[class_id.Fri]
+          } else {
+            fri_color = col_array[class_id.Fri]
+          }
+
+          past_classId.Fri = class_id.Fri
+
+          if (!hrs.Fri) {
+            hr_percent.Fri = convertHrs(pasthrs.Fri, 60, '00')
+            pasthrs.Fri = hr_percent.Fri.third
+          } else {
+            hr_percent.Fri = convertHrs(pasthrs.Fri, hrs.Fri, mins.Fri)
+            pasthrs.Fri = hr_percent.Fri.third
+          }
+
+        } else {
+          if (pasthrs.Fri !== 0) {
+            fri_color1 = col_array[past_classId.Fri]
+          }
+
+          hr_percent.Fri = convertHrs(pasthrs.Fri, 60, '00')
+          pasthrs.Fri = hr_percent.Fri.third
         }
 
         return (
           <tr className={col_content} key={time.slice(0, time.search(/[:]/g))}>
             <td className={col_content}>{time}</td>
-            <td style={{background: mon_color}} className={col_content}>{Mon}</td>
-            <td style={{background: tues_color}} className={col_content}>{Tues}</td>
-            <td style={{background: wed_color}} className={col_content}>{Wed}</td>
-            <td style={{background: thurs_color}} className={col_content}>{Thurs}</td>
-            <td style={{background: fri_color}} className={col_content}>{Fri}</td>
+            <td style={{background: `linear-gradient(to bottom, ${mon_color1} ${hr_percent.Mon.first}, ${mon_color} ${hr_percent.Mon.first} ${hr_percent.Mon.second}, ${mon_color2} ${hr_percent.Mon.second} )`}} className={col_content}>{Mon}</td>
+            <td style={{background: `linear-gradient(to bottom, ${tues_color1} ${hr_percent.Tues.first}, ${tues_color} ${hr_percent.Tues.first} ${hr_percent.Tues.second},${tues_color2} ${hr_percent.Tues.second} )`}} className={col_content}>{Tues}</td>
+            <td style={{background: `linear-gradient(to bottom, ${wed_color1} ${hr_percent.Wed.first}, ${wed_color} ${hr_percent.Wed.first} ${hr_percent.Wed.second}, ${wed_color2} ${hr_percent.Wed.second} )`}} className={col_content}>{Wed}</td>
+            <td style={{background: `linear-gradient(to bottom, ${thurs_color1} ${hr_percent.Thurs.first}, ${thurs_color} ${hr_percent.Thurs.first} ${hr_percent.Thurs.second}, ${thurs_color2} ${hr_percent.Thurs.second} )`}} className={col_content}>{Thurs}</td>
+            <td style={{background: `linear-gradient(to bottom, ${fri_color1} ${hr_percent.Fri.first}, ${fri_color} ${hr_percent.Fri.first} ${hr_percent.Fri.second}, ${fri_color2} ${hr_percent.Fri.second} )`}} className={col_content}>{Fri}</td>
           </tr>
         )
       })
